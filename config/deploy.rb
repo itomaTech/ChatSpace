80b5a966609206ceccaab64ba3a91c843d10d557
itomaTech <tomoyakadowaki.1211@gmail.com>
2019-04-16 16:19:58 +0900

    Tweak

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "ChatSpace"
-set :repo_url, "git@example.com:itomaTech/ChatSpace.git"
+set :repo_url, "git@github.com:itomaTech/ChatSpace.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
